9ef8a3a0e2a53c9d0457d2233e6e5245b3809dd2
FelicityOdera <87133135+FelicityOdera@users.noreply.github.com>
2022-07-12 22:28:00 +0300

    print_d.c file added

--- print_d.c
+++ print_d.c
@@ -6,7 +6,7 @@
  * @i: base 10 integer to print
  * Return: number of printed digits
  */
-int print_d(va_list i)
+int print_d(va_list d)
 {
 	int a[10];
 	int j = 1, m = 1000000000, n, sum = 0, counter = 0;
